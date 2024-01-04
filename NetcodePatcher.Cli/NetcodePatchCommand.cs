23527944f6671aa093b825e5e7286f3917c7ba24
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-04 09:19:57 +0000

    allow configuring logfile and log-level

--- NetcodePatcher.Cli/NetcodePatchCommand.cs
+++ NetcodePatcher.Cli/NetcodePatchCommand.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Threading.Tasks;
 using Serilog;
+using Serilog.Events;
 
 namespace NetcodePatcher.Cli;
 
@@ -23,12 +24,23 @@ public sealed class NetcodePatchCommand : RootCommand
         Add(new Option<string?>(["--output", "-o"], "Output folder/file path").LegalFilePathsOnly());
         Add(new Option<bool>("--no-overwrite", "Sets output path to [assembly]_patched.dll, as opposed to renaming the original assembly").LegalFilePathsOnly());
         Add(new Option<bool>("--disable-parallel", "Don't publicize in parallel"));
+        Add(new Option<LogEventLevel>("--log-level", () => LogEventLevel.Information, "Sets the minimum log-level. Messages below this are ignored."));
+        Add(new Option<string?>("--log-file", "Set a filepath to log to.") { Arity = ArgumentArity.ExactlyOne }.LegalFilePathsOnly());
         
         Handler = HandlerDescriptor.FromDelegate(Handle).GetCommandHandler();
     }
 
-    private static void Handle(FileSystemInfo plugin, FileSystemInfo[] dependencies, string? output, bool noOverwrite, bool disableParallel)
+    private static void Handle(FileSystemInfo plugin, FileSystemInfo[] dependencies, string? output, bool noOverwrite, bool disableParallel, LogEventLevel minimumLogEventLevel, string? logFile)
     {
+        var logConfiguration = new LoggerConfiguration()
+            .MinimumLevel.Is(minimumLogEventLevel)
+            .WriteTo.Console();
+
+        if (logFile is not null)
+            logConfiguration = logConfiguration.WriteTo.File(logFile, rollingInterval: RollingInterval.Infinite);
+
+        Log.Logger = logConfiguration.CreateLogger();
+        
         Log.Information("Initializing NetcodePatcher v{Version}", Assembly.GetExecutingAssembly().GetName().Version);
 
         Log.Debug("Provided 'plugins' input: {Plugins}", plugin);
