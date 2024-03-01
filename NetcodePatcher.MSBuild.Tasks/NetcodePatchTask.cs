e583821a7bab5e6f596bacf7a5f1c07c3e556490
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-03-01 20:51:57 +0000

    separate the task source from the msbuild integration

--- NetcodePatcher.MSBuild.Tasks/NetcodePatchTask.cs
+++ NetcodePatcher.MSBuild.Tasks/NetcodePatchTask.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 using Serilog;
@@ -12,7 +13,7 @@ namespace NetcodePatcher.MSBuild;
 
 public class NetcodePatchTask : Task
 {
-    [Required] 
+    [Required]
     public ITaskItem[] Patch { get; set; } = null!;
 
     [Required]
@@ -21,40 +22,40 @@ public class NetcodePatchTask : Task
     public string? NoOverwrite { get; set; }
 
     public string? DisableParallel { get; set; }
-    
+
     public override bool Execute()
     {
         Serilog.Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()
-            .WriteTo.TaskLoggingHelper(Log)
+            .WriteTo.MSBuildTask(this)
             .CreateLogger();
-        
+
         var toolVersion = typeof(NetcodePatchTask).Assembly
             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
             .InformationalVersion;
         Serilog.Log.Information("Initializing NetcodePatcher v{Version:l}", toolVersion);
-        
+
         var noOverwrite = false;
         if (!string.IsNullOrEmpty(NoOverwrite))
         {
             noOverwrite = bool.Parse(NoOverwrite);
         }
-        
+
         var disableParallel = false;
         if (!string.IsNullOrEmpty(DisableParallel))
         {
             disableParallel = bool.Parse(DisableParallel);
         }
-        
+
         var stopwatch = Stopwatch.StartNew();
-        
+
         void RunPatch(ITaskItem patchSpecifier)
         {
             var pluginAssembly = new FileInfo(patchSpecifier.ItemSpec);
-            
+
             var inputPath = pluginAssembly.FullName;
             var outputPath = pluginAssembly.DirectoryName!;
-            
+
             if (patchSpecifier.GetMetadata("OutputPath") is { } rawOutput && !string.IsNullOrEmpty(rawOutput))
             {
                 if (rawOutput.IndexOfAny(Path.GetInvalidPathChars()) != -1)
@@ -68,13 +69,13 @@ public class NetcodePatchTask : Task
                 Serilog.Log.Information("Skipping : {FileName}", pluginAssembly.Name);
                 return;
             }
-            
+
             if (Directory.Exists(outputPath) || string.IsNullOrEmpty(Path.GetExtension(outputPath)))
             {
                 Directory.CreateDirectory(outputPath);
                 outputPath = Path.Combine(outputPath, noOverwrite ? $"{Path.GetFileNameWithoutExtension(pluginAssembly.Name)}_patched{Path.GetExtension(pluginAssembly.Name)}" : pluginAssembly.Name);
             }
-            
+
             Patcher.Patch(inputPath, outputPath, ReferenceAssemblyPaths.Select(info => info.ItemSpec).ToArray());
         }
 
@@ -97,10 +98,35 @@ public class NetcodePatchTask : Task
             Serilog.Log.Fatal(exception, "Netcode patching failed!");
             return false;
         }
-        
+
         stopwatch.Stop();
         Serilog.Log.Information("Done in {Time}", stopwatch.Elapsed);
-        
+
         return true;
+    }
+
+    private static MethodInfo LoadPatchMethodForNetcodeVersion(string netcodeVersion)
+    {
+        AssemblyLoadContext patcherLoadContext = new AssemblyLoadContext("PatcherLoadContext");
+        Assembly patcherAssembly;
+        try {
+            patcherAssembly = patcherLoadContext.LoadFromAssemblyName(new AssemblyName($"NetcodePatcher.nv{netcodeVersion}"));
+        }
+        catch (FileNotFoundException exc) {
+            throw new ArgumentException($"The supplied Unity Netcode for GameObjects version '{netcodeVersion}' is either unknown or unsupported.", exc);
+        }
+        catch (Exception exc) {
+            throw new ArgumentException($"Failed to load patcher for Netcode {netcodeVersion}", exc);
+        }
+
+        var patcherType = patcherAssembly
+            .GetTypes()
+            .First(t => t is { IsPublic: true, Name: "Patcher" });
+
+        var patcherPatchMethod = patcherType.GetMethod("Patch", BindingFlags.Public | BindingFlags.Static);
+        if (patcherPatchMethod is null)
+            throw new Exception("Failed to find `public static` `Patch` member in loaded patcher Type.");
+
+        return patcherPatchMethod;
     }
 }
